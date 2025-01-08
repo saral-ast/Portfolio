f5569c8a9a63b727111bbf96417c1e96f10ea5ce
saral-ast <saralchauhan.ast@gmail.com>
2025-01-08 14:34:48 +0530

    Contact me commit

---
+++ Media/9f86324e-27db-4888-84fe-fea89127dfbf/config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":1,"title":"21it020.docx","author":"","subject":"","keywords":"","creator":"","producer":"Skia/PDF m128 Google Docs Renderer","creationdate":"","moddate":"","trapped":"","fileName":"Saral-Resume.pdf","bounds":[[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
